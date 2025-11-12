2d53c81843048671bb1da4e4d1244a0a6a82e1ed
Harshal-XRstudio <harshal.xrstudio@gmail.com>
2025-11-12 18:22:35 +0530

    add shadcn

--- src/components/home/homePage.tsx
+++ src/components/home/homePage.tsx
@@ -1,10 +1,12 @@
 "use client";
 import React from 'react'
+import { Button } from '../ui/button';
 
 const HomePage = () => {
   return (
     <div className='w-full h-full text-black dark:text-white'>
       <h1 className='text-black dark:text-white'>Home Page</h1>
+      <Button variant='outline'>Click me</Button>
     </div>
   )
 }
